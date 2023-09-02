b5c47d131acdbfb50c7d7daeee8f5866972de1a8
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-09-01 18:36:22 -0700

    refactor: index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
-const wightColor1 = document.getElementById("color1");
-const wightColor2 = document.getElementById("color2");
-const wightColor3 = document.getElementById("color3");
 const btnChangeColor = document.querySelector("#js");
 
-btnChangeColor.addEventListener("change", (event) => {
+btnChangeColor.addEventListener("change", () => {
+  const colors = document.querySelectorAll(".color");
+
   if (btnChangeColor.checked) {
-    wightColor1.classList.add("green");
-    wightColor2.classList.add("green");
-    wightColor3.classList.add("green");
+    colors.forEach((color) => {
+      color.classList.add("green");
+    });
   } else {
-    wightColor1.classList.remove("green");
-    wightColor2.classList.remove("green");
-    wightColor3.classList.remove("green");
+    colors.forEach((color) => {
+      color.classList.remove("green");
+    });
   }
 });
